8f87f51b456d51a6694868ab5b0bf11f43a36803
vikalatosh <latosh.viktorija@mail.ru>
2021-05-16 12:42:24 +0300

    commit BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,21 +1,30 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
     WebDriver driver;
+    ChromeOptions options;
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
+        options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", System.getProperty("user.dir"));
+        options.setExperimentalOption("prefs", chromePrefs);
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //неявные ожидания
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterMethod(alwaysRun = true)
