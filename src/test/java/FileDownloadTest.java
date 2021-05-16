ebcbf89891fe788ca34861c24af103f9a120e2d4
vikalatosh <latosh.viktorija@mail.ru>
2021-05-16 12:45:30 +0300

    added Thread.sleep

--- src/test/java/FileDownloadTest.java
+++ src/test/java/FileDownloadTest.java
@@ -3,29 +3,25 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.util.HashMap;
 
 import static org.testng.Assert.assertTrue;
 
 public class FileDownloadTest extends BaseTest {
 
     @Test
-    public void fileDownload() {
-        HashMap<String, Object> chromePrefs = new HashMap<>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", System.getProperty("user.dir"));
-        options.setExperimentalOption("prefs", chromePrefs);
+    public void fileDownload() throws InterruptedException {
         driver.get("https://www.thinkbroadband.com/download");
-        WebElement file = driver.findElement(By.xpath("//div[text()='Extra Small File (5MB)']//following::p/a[text()='80']"));
+        WebElement file;
+        file = driver.findElement(By.xpath("//div[text()='Extra Small File (5MB)']//following::p/a[text()='80']"));
         file.click();
         driver.navigate().refresh();
         file = driver.findElement(By.xpath("//div[text()='Extra Small File (5MB)']//following::p/a[text()='80']"));
         file.click();
+        Thread.sleep(15000);
         File folder = new File(System.getProperty("user.dir"));
         File[] listOfFiles = folder.listFiles();
         boolean found = false;
         File f = null;
-
         //Look for the file in the files
         // You should write smart REGEX according to the filename
         for (File listOfFile : listOfFiles) {
